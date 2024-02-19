bacca875a5d56f997191383e20238165d89a31f3
Aneta Muslić <Aneta.Muslic@blu.com>
2024-02-19 16:00:19 +0100

    added settings for Docker env

--- ApiApplication/Application/Exceptions/ValidationException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiApplication.Application.Exceptions
-{
-    public class ValidationException: BaseException
-    {
-        public ValidationException(int statusCode, string? message) : base(statusCode, message ?? string.Empty)
-        {
-        }
-    }
-}
